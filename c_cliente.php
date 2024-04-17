67cf8a460fe7ff6123a60c1026d13983d73a547d
camili <camili.costa07@gmail.com>
2024-04-17 20:24:28 -0300

    ajustes finais

--- c_cliente.php
+++ c_cliente.php
@@ -4,7 +4,6 @@
 //variável só recebe um valor por vez
  
 $cliente = $_POST['cliente'];
-$email = $_POST['email'];
 $endereço = $_POST['endereço'];
 $nr_endereço = $_POST['nr_endereço'];
 $cpf = $_POST['cpf'];
@@ -12,17 +11,10 @@ $cnpj = $_POST['cnpj'];
 $Telefone = $_POST['Telefone'];
  
  
-include 'conexão.php';
+include 'conexao.php';
 
 //Dados para inserir os dados
-$insert = "INSERT INTO tb_cliente VALUES (NULL,'$cliente','$email','$endereço','$nr_endereço','$cpf' '$cnpj', '$Telefone')";
+$insert = "INSERT INTO tb_cliente VALUES (NULL,'$cliente','$endereço','$nr_endereço','$cpf' '$cnpj', '$Telefone')";
 
-//inserindo os dados no banco de dados utilizando a função mysqli
 
-$query = mysqli_query($conexao, $insert);
-
-echo "Inserido com sucesso";
- 
- 
- 
 ?>
