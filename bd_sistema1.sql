9c1d8346d5451db9be8e9475231324fc34c9c36f
camili <camili.costa07@gmail.com>
2024-06-24 20:15:20 -0300

    finalizado

--- bd_sistema1.sql
+++ bd_sistema1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 18-Abr-2024 às 02:12
+-- Tempo de geração: 25-Jun-2024 às 01:11
 -- Versão do servidor: 8.0.27
 -- versão do PHP: 8.2.0
 
@@ -36,6 +36,35 @@ CREATE TABLE `tb_cliente` (
   `nr_cnpj` char(14) NOT NULL,
   `nr_telefone` char(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Extraindo dados da tabela `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`cd_cliente`, `nm_cliente`, `nm_endereço`, `nr_endereço`, `nr_cpf`, `nr_cnpj`, `nr_telefone`) VALUES
+(1, 'Camili Costa', 'Avenida Da escola', 12, '123124343', '12312234', '123123434');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_editora`
+--
+
+CREATE TABLE `tb_editora` (
+  `cd_editora` int NOT NULL,
+  `nm_editora_edi` varchar(45) NOT NULL,
+  `nm_enderec_edi` varchar(45) NOT NULL,
+  `nr_endereco_edi` varchar(45) NOT NULL,
+  `nr_cnpj_edi` text NOT NULL,
+  `nr_telefone_edi` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `tb_editora`
+--
+
+INSERT INTO `tb_editora` (`cd_editora`, `nm_editora_edi`, `nm_enderec_edi`, `nr_endereco_edi`, `nr_cnpj_edi`, `nr_telefone_edi`) VALUES
+(1, 'Companhia de letras', 'Avenida dos Bandeiranteres', '2', '121334435', '12312432455');
 
 -- --------------------------------------------------------
 
@@ -70,6 +99,12 @@ ALTER TABLE `tb_cliente`
   ADD PRIMARY KEY (`cd_cliente`);
 
 --
+-- Índices para tabela `tb_editora`
+--
+ALTER TABLE `tb_editora`
+  ADD PRIMARY KEY (`cd_editora`);
+
+--
 -- Índices para tabela `tb_user`
 --
 ALTER TABLE `tb_user`
@@ -83,7 +118,13 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT de tabela `tb_cliente`
 --
 ALTER TABLE `tb_cliente`
-  MODIFY `cd_cliente` int NOT NULL AUTO_INCREMENT;
+  MODIFY `cd_cliente` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tb_editora`
+--
+ALTER TABLE `tb_editora`
+  MODIFY `cd_editora` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `tb_user`
